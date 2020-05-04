74b44819866271e6a68b660257c5c567b0ce92bd
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2020-05-04 13:15:30 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const passport = require('passport');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const port = 3200;
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => console.log(`Server listening at: http://localhost:${port}`));
+module.exports = app;
