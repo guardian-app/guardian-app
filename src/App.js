bb15f8d629fb18d19710058f7d59144a31b3f366
Ridmi <ridmiashi@gmail.com>
2020-06-05 00:09:45 +0530

    add project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Dashboard from './Dashboard';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+     <Route path="/dashboard" component={Dashboard}/> 
+     <Route path="/signup" component={SignUp}/> 
+     <Route path="/signin" component={SignIn}/>
+     
+    </div>
+    </Router>
+  );
+}
+
+export default App;
+ 
