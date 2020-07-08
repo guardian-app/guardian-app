cafb8281eefe0cb3bc0d9f86b94869ce4d75fbf3
chehanilakshika <balclakshi@gmail.com>
2020-07-08 15:46:17 +0530

    New changes

--- components/trustedContacts/Trustedconform.js
+++ components/trustedContacts/Trustedconform.js
@@ -5,6 +5,44 @@ import {Actions, ActionConst} from 'react-native-router-flux';
 import t from 'tcomb-form-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
+export default class App extends Component { 
+
+ 
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView  style = {{paddingTop: 20}}>
+            <Form 
+            ref={c => this._form = c}
+            type={User} 
+            options={options}
+            value ={values}
+            />
+
+            <View style = {{paddingTop: 20}}>
+              <Button
+              
+              color="#0020C2"
+              title="OK"
+              onPress={()=> {Actions.HomeScreen()}}
+              />
+            </View>
+
+            <View style = {{paddingTop: 20, paddingBottom: 40}}>
+              <Button
+              color="#800080"
+              title="UPDATE"
+              //onPress={this.handleSubmit}
+              />
+            </View>
+            
+        </ScrollView>
+        
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
