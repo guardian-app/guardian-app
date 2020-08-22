fb6bfe6b1896b39b19409e228cb4d785f3df5604
pramithvidusara@gmail.com <43032202+TilangaPramith@users.noreply.github.com>
2020-08-23 01:35:23 +0530

    edit

--- components/contact/addContact.js
+++ components/contact/addContact.js
@@ -1,208 +1,273 @@
-import React, { Component } from 'react';
-import { View, StyleSheet, Button,Alert,Animated,Easing } from 'react-native';
-import {Actions, ActionConst} from 'react-native-router-flux';
+// import React, { Component } from 'react';
+// import { View, StyleSheet, Button,Alert,Animated,Easing } from 'react-native';
+// import {Actions, ActionConst} from 'react-native-router-flux';
 
-import t from 'tcomb-form-native';
-import { ScrollView } from 'react-native-gesture-handler';
+// import t from 'tcomb-form-native';
+// import { ScrollView } from 'react-native-gesture-handler';
 
-const Form = t.form.Form;
+// const Form = t.form.Form;
 
-const Email = t.refinement(t.String, (email) => {
-    const reg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/; //or any other regexp
-    return reg.test(email);
-});
+// const Email = t.refinement(t.String, (email) => {
+//     const reg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/; //or any other regexp
+//     return reg.test(email);
+// });
 
-const Password = t.refinement(t.String, (password) => {
-    const reg = /^(?=\S+$).{8,}$/;
-    console.log(reg.test(password))
-    return reg.test(password);
-}); 
+// const Password = t.refinement(t.String, (password) => {
+//     const reg = /^(?=\S+$).{8,}$/;
+//     console.log(reg.test(password))
+//     return reg.test(password);
+// }); 
 
-const Mobile = t.refinement(t.String, (mobile)=>{
-    const reg = /^\d{10}$/;
-    return reg.test(mobile);
-});
+// const Mobile = t.refinement(t.String, (mobile)=>{
+//     const reg = /^\d{10}$/;
+//     return reg.test(mobile);
+// });
 
 
-const User = t.struct({
-   FirstName: t.String,
-  LastName: t.String,
-  email: Email,
-  relationship: t.String,
-  mobile: Mobile,
-  address: t.String,
-});
+// const User = t.struct({
+//    FirstName: t.String,
+//   LastName: t.String,
+//   email: Email,
+//   relationship: t.String,
+//   mobile: Mobile,
+//   address: t.String,
+// });
 
-const formStyles = {
-  ...Form.stylesheet,
-  formGroup: {
-    normal: {
-      marginBottom: 10
-    },
-  },
-  controlLabel: {
-    normal: {
-      color: 'blue',
-      fontSize: 18,
-      marginBottom: 7,
-      fontWeight: '600'
-    },
-    // the style applied when a validation error occours
-    error: {
-      color: 'red',
-      fontSize: 18,
-      marginBottom: 7,
-      fontWeight: '600'
-    }
-  }
-}
+// const formStyles = {
+//   ...Form.stylesheet,
+//   formGroup: {
+//     normal: {
+//       marginBottom: 10
+//     },
+//   },
+//   controlLabel: {
+//     normal: {
+//       color: 'blue',
+//       fontSize: 18,
+//       marginBottom: 7,
+//       fontWeight: '600'
+//     },
+//     // the style applied when a validation error occours
+//     error: {
+//       color: 'red',
+//       fontSize: 18,
+//       marginBottom: 7,
+//       fontWeight: '600'
+//     }
+//   }
+// }
 
-const options = {
-  fields: {
-    FirstName : {
-        error: 'provide your first name'
-    }  ,
-    LastName : {
-        error: 'provide your last name'
-    }  ,
-    email: {
-      error: 'Without an email address how are you going to reset your password when you forget it?'
-    },
-    mobile: {
-        keyboardType:'numeric',
-        error: 'Please, provide correct phone number',
-    },
-    address: {
-      error: 'Please provide Address'
-    },
-    phoneNumber: {
-        error: 'Please enter valid phone number'
-    },
-    relationship: {
-        error: 'Relationship can not be empty'
-    }
-  },
-  stylesheet: formStyles,
-};
+// const options = {
+//   fields: {
+//     FirstName : {
+//         error: 'provide your first name'
+//     }  ,
+//     LastName : {
+//         error: 'provide your last name'
+//     }  ,
+//     email: {
+//       error: 'Without an email address how are you going to reset your password when you forget it?'
+//     },
+//     mobile: {
+//         keyboardType:'numeric',
+//         error: 'Please, provide correct phone number',
+//     },
+//     address: {
+//       error: 'Please provide Address'
+//     },
+//     phoneNumber: {
+//         error: 'Please enter valid phone number'
+//     },
+//     relationship: {
+//         error: 'Relationship can not be empty'
+//     }
+//   },
+//   stylesheet: formStyles,
+// };
 
-const _onHome = () =>{
+// const _onHome = () =>{
 
-  setTimeout(() => {
-    Actions.HomeScreen();
-  }, 2300);
-}
+//   setTimeout(() => {
+//     Actions.HomeScreen();
+//   }, 2300);
+// }
 
-const _onAlert=()=> {
-  setTimeout(() => {
-    //Actions.ChildRegScreen();
-  }, 2300);
-}
+// const _onAlert=()=> {
+//   setTimeout(() => {
+//     //Actions.ChildRegScreen();
+//   }, 2300);
+// }
 
-export default class App extends Component { 
+// export default class App extends Component { 
 
-  handleSubmit = () => {
-    const value = this._form.getValue();
+//   handleSubmit = () => {
+//     const value = this._form.getValue();
 
-    var token = localStorage.getItem("key");
+//     var token = localStorage.getItem("key");
     
-    ///fetch eka
+//     ///fetch eka
 
-    fetch("http://192.168.43.133:3000/users/createContact",{
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": "Bearer "+token,
-      },
-      body: JSON.stringify({
-        email_address : value.email,
-        first_name: value.FirstName,
-        last_name: value.LastName,
-        address: value.address,
-        phone_number: value.mobile,
-        relationship: value.relationship
-      })
+//     fetch("http://192.168.43.133:3000/users/createContact",{
+//       method: "POST",
+//       headers: {
+//         "Accept": "application/json",
+//         "Content-Type": "application/json",
+//         "Authorization": "Bearer "+token,
+//       },
+//       body: JSON.stringify({
+//         email_address : value.email,
+//         first_name: value.FirstName,
+//         last_name: value.LastName,
+//         address: value.address,
+//         phone_number: value.mobile,
+//         relationship: value.relationship
+//       })
 
-    })
-    .then(function(response) {
+//     })
+//     .then(function(response) {
       
-      if(response.status == 201){
-        console.log('done')
-        Alert.alert(
-          "Insertion Success",
-          "Your trusted contact added",
-          [
-            { text: "OK", onPress: _onHome() }
-          ]
-        )
-      }
-      else if(response.status == 409){
-        Alert.alert(
-          "Insertion Failed!",
-          "Email is already used",
-          [
-            { text: "OK",onPress: _onAlert()  }
-          ]
-        )
-      } 
-      else if(response.status == 401){
-        Alert.alert(
-          "Insertion failed Failed!",
-          "Not found",
-          [
-            { text: "OK",onPress: _onAlert()  }
-          ]
-        )
-      }
-      else if(response.status == 403){
-        Alert.alert(
-          "Insertion Failed!",
-          "You are not parent",
-          [
-            { text: "OK",onPress: _onAlert()  }
-          ]
-        )
-      }  
-      else{
-        return response;
-      }
+//       if(response.status == 201){
+//         console.log('done')
+//         Alert.alert(
+//           "Insertion Success",
+//           "Your trusted contact added",
+//           [
+//             { text: "OK", onPress: _onHome() }
+//           ]
+//         )
+//       }
+//       else if(response.status == 409){
+//         Alert.alert(
+//           "Insertion Failed!",
+//           "Email is already used",
+//           [
+//             { text: "OK",onPress: _onAlert()  }
+//           ]
+//         )
+//       } 
+//       else if(response.status == 401){
+//         Alert.alert(
+//           "Insertion failed Failed!",
+//           "Not found",
+//           [
+//             { text: "OK",onPress: _onAlert()  }
+//           ]
+//         )
+//       }
+//       else if(response.status == 403){
+//         Alert.alert(
+//           "Insertion Failed!",
+//           "You are not parent",
+//           [
+//             { text: "OK",onPress: _onAlert()  }
+//           ]
+//         )
+//       }  
+//       else{
+//         return response;
+//       }
 
-    }).then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      console.log('Request succeeded with JSON response:', json);
-    }).catch(function(error) {
-      console.log('Request failed:', error);
-    });
+//     }).then(function(response) {
+//       return response.json();
+//     }).then(function(json) {
+//       console.log('Request succeeded with JSON response:', json);
+//     }).catch(function(error) {
+//       console.log('Request failed:', error);
+//     });
 
-  }
+//   }
   
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <ScrollView>
+//             <Form 
+//             ref={c => this._form = c}
+//             type={User} 
+//             options={options}
+//             />
+//             <Button
+//             color="#0020C2"
+//             title="Add Contact"
+//             onPress={this.handleSubmit}
+//             />
+//         </ScrollView>
+        
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     justifyContent: 'center',
+//     marginTop: 50,
+//     padding: 20,
+//     backgroundColor: '#ffffff',
+//   },
+// });
+
+
+//////////////////////////
+import React, { Component } from "react"
+import { View, Button } from "react-native"
+
+import TextField from "textfield"
+import validation from "validation"
+import validate from "validation_wrapper"
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: "",
+      emailError: "",
+      password: "",
+      passwordError: ""
+    }
+  }
+
+  register() {
+    const emailError = validate("email", this.state.email)
+    const passwordError = validate("password", this.state.password)
+
+    this.setState({
+      emailError: emailError,
+      passwordError: passwordError
+    })
+
+    if (!emailError && !passwordError) {
+      alert("Details are valid!")
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView>
-            <Form 
-            ref={c => this._form = c}
-            type={User} 
-            options={options}
-            />
-            <Button
-            color="#0020C2"
-            title="Add Contact"
-            onPress={this.handleSubmit}
-            />
-        </ScrollView>
-        
+      <View>
+        <TextField
+          onChangeText={(value) => this.setState({ email: value.trim() })}
+          onBlur={() => {
+            this.setState({
+              emailError: validate("email", this.state.email)
+            })
+          }}
+          error={this.state.emailError}
+        />
+
+        <TextField
+          onChangeText={(value) => this.setState({ password: value.trim() })}
+          onBlur={() => {
+            this.setState({
+              passwordError: validate("password", this.state.password)
+            })
+          }}
+          error={this.state.passwordError}
+          secureTextEntry={true}
+        />
+
+        <Button title="Register" onPress={this.validateRegister} />
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    marginTop: 50,
-    padding: 20,
-    backgroundColor: '#ffffff',
-  },
-});
