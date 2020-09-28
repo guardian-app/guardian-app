58355755fa1ba1d8c1604d5e6038415cb2037f92
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2020-09-28 23:07:52 +0530

    Date selection

--- src/scenes/ChildMapScene.tsx
+++ src/scenes/ChildMapScene.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import * as SecureStore from 'expo-secure-store';
 import { useSelector } from 'react-redux';
-import { Appbar } from 'react-native-paper';
+import { Appbar, Menu, Divider } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, ScrollView, View } from 'react-native';
 import { Snackbar } from 'react-native-paper';
@@ -31,47 +31,116 @@ type Coords = {
     id: number
 }
 
+type DateSelection = "real-time" | Date;
+
 const ChildMap = ({ route, navigation }: Props) => {
     const { user_id } = route.params;
     const child = useSelector((state: any) => state.childReducer.children.find((child: any) => child.user_id === user_id));
 
+    const [dateSelection, setDateSelection] = useState<DateSelection>("real-time");
     const [markers, setMarkers] = useState<Coords[]>([]);
-    const [visible, setVisible] = useState(false);
+    const [snackbarVisible, setSnackbarVisible] = useState(false);
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    const openMenu = () => setMenuVisible(true);
+    const closeMenu = () => setMenuVisible(false);
 
     useEffect(() => {
         (async () => {
-            try {
-                const token = await SecureStore.getItemAsync("token");
-                const request = fetch(
-                    `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/location/${user_id}`,
-                    {
-                        method: "GET",
-                        headers: { 'Content-Type': 'application/json', Accept: 'application/json', Authorization: `Bearer ${token}` },
-                    }
-                );
+            if (dateSelection === "real-time") {
+                try {
+                    const token = await SecureStore.getItemAsync("token");
+                    const request = fetch(
+                        `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/location/${user_id}`,
+                        {
+                            method: "GET",
+                            headers: { 'Content-Type': 'application/json', Accept: 'application/json', Authorization: `Bearer ${token}` },
+                        }
+                    );
 
-                const response = await request;
-                if (response.ok) {
-                    const object = await response.json();
-                    setMarkers([{
-                        coordinate: {
-                            latitude: parseFloat(object.latitude),
-                            longitude: parseFloat(object.longitude)
-                        },
-                        title: new Date(object.timestamp).toDateString(),
-                        description: new Date(object.timestamp).toTimeString(),
-                        id: object.timestamp
-                    }]);
-                } else {
-                    setVisible(true);
+                    const response = await request;
+                    if (response.ok) {
+                        const object = await response.json();
+                        setMarkers([{
+                            coordinate: {
+                                latitude: parseFloat(object.latitude),
+                                longitude: parseFloat(object.longitude)
+                            },
+                            title: new Date(object.timestamp).toDateString(),
+                            description: new Date(object.timestamp).toTimeString(),
+                            id: object.timestamp + object.longitude + object.latitude
+                        }]);
+                    } else {
+                        setMarkers([]);
+                        setSnackbarVisible(true);
+                    };
+                } catch (err) {
+                    setMarkers([]);
+                    console.warn(err);
                 };
-            } catch (err) {
-                console.warn(err);
-            };
+            } else {
+                try {
+                    const token = await SecureStore.getItemAsync("token");
+                    const request = fetch(
+                        `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/location/${user_id}/history/${dateSelection.toISOString().split('T')[0]}`,
+                        {
+                            method: "GET",
+                            headers: { 'Content-Type': 'application/json', Accept: 'application/json', Authorization: `Bearer ${token}` },
+                        }
+                    );
+
+                    const response = await request;
+                    if (response.ok) {
+                        const data: any[] = await response.json();
+                        setMarkers(data.map((object: any) => {
+                            return {
+                                coordinate: {
+                                    latitude: parseFloat(object.latitude),
+                                    longitude: parseFloat(object.longitude)
+                                },
+                                title: new Date(object.timestamp).toDateString(),
+                                description: new Date(object.timestamp).toTimeString(),
+                                id: object.timestamp
+                            }
+                        }));
+                    } else {
+                        setMarkers([]);
+                        setSnackbarVisible(true);
+                    };
+                } catch (err) {
+                    setMarkers([]);
+                    console.warn(err);
+                };
+            }
         })();
-    }, []);
+    }, [dateSelection]);
 
     const _handleMore = () => console.log('Shown more');
+
+    const dateSelectionMenuItems = [];
+
+    const todayDate = new Date();
+    for (let i = 0; i < 7; i++) {
+        const currentDate = new Date();
+        currentDate.setDate(todayDate.getDate() - i);
+
+        dateSelectionMenuItems.push(
+            <Menu.Item
+                onPress={() => {
+                    setDateSelection(currentDate);
+                    setMenuVisible(false);
+                }}
+                title={
+                    i === 0
+                        ? `Today (${currentDate.toDateString()})`
+                        : i === 1
+                            ? `Yesterday (${currentDate.toDateString()})`
+                            : `${currentDate.toDateString()}`
+                }
+                key={currentDate.toString()}
+            />
+        );
+    };
 
     return (
         <>
@@ -102,12 +171,39 @@ const ChildMap = ({ route, navigation }: Props) => {
                     })}
                 </MapView>
 
+                <Appbar style={styles.bottom}>
+                    <Appbar.Content
+                        title={dateSelection === "real-time"
+                            ? "Real-time"
+                            : dateSelection.getUTCDate() === (new Date()).getUTCDate()
+                                ? "Today"
+                                : dateSelection.getUTCDate() == (new Date(Date.now() - 864e5)).getUTCDate()
+                                    ? "Yesterday"
+                                    : dateSelection.toISOString().split('T')[0]}
+                        subtitle={'Last seen 1 hour ago'}
+                    />
+
+                    <Menu
+                        visible={menuVisible}
+                        onDismiss={closeMenu}
+                        anchor={<Button onPress={openMenu}>Show menu</Button>}>
+                        <Menu.Item onPress={() => {
+                            setDateSelection("real-time");
+                            setMenuVisible(false);
+                        }} title="Real-time" />
+                        <Divider />
+                        {dateSelectionMenuItems}
+                    </Menu>
+
+                    <Appbar.Action icon="dots-vertical" onPress={openMenu} />
+                </Appbar>
+
                 <Snackbar
-                    visible={visible}
-                    onDismiss={() => setVisible(false)}
+                    visible={snackbarVisible}
+                    onDismiss={() => setSnackbarVisible(false)}
                     action={{
                         label: 'OK',
-                        onPress: () => setVisible(false)
+                        onPress: () => setSnackbarVisible(false)
                     }}>
                     {"No location data available at the moment. Make sure you setup the Guardian application on your child's mobile device."}
                 </Snackbar>
@@ -129,6 +225,12 @@ const styles = StyleSheet.create({
     mapStyle: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
+    },
+    bottom: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
     },
 });
 
