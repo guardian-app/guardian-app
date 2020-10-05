02fc715d133174c96585f23a76ff4ee9d0ae22f0
Lakma Chehani <43143859+chehanilakshika@users.noreply.github.com>
2020-10-05 16:26:27 +0530

    trusted contact services

---
+++ services/contact.js
@@ -0,0 +1,31 @@
+const database = require('../config/database');
+
+// const selectChildren = () => {
+//     return database.execute(
+//         "SELECT `user_id`, `email_address`, `first_name`, `last_name`, `address`, `phone_number` FROM `user` WHERE user.role = 'child'",
+//     );
+// };
+
+const selectByContactId = (contact_id) => {
+    return database.execute(
+        "SELECT `contact_id`, `email_address`, `first_name`, `last_name`, `address`, `phone_number`,`relationship` FROM `trusted_contact` WHERE parent_id = ? LIMIT 1",
+        [contact_id]
+    );
+};
+
+const selectContactByParentId = (parent_id) => {
+    return database.execute(
+        "SELECT `contact_id`, `email_address`, `first_name`, `last_name`, `address`, `phone_number`,`relationship` FROM `trusted_contact` WHERE parent_id = ?",
+        //(SELECT `parent_id` FROM `user` WHERE user.user_id = ? LIMIT 1) AND user.role = 'parent' LIMIT 1
+        [parent_id]
+    );
+};
+
+const insertContact = (contact) => {
+    return database.execute(
+        'INSERT INTO `trusted_contact` (`email_address`, `first_name`, `last_name`, `address`, `phone_number`,`relationship`, `parent_id`)  VALUES (?, ?, ?, ?, ?, ?,?)',
+        [contact.email_address, contact.first_name, contact.last_name, contact.address, contact.phone_number,contact.relationship, contact.parent_id]
+    );
+};
+
+module.exports = { selectByContactId, selectContactByParentId, insertContact };
