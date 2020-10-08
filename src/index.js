1ba354c78299f5c1881a187a90e14b85dd46d08a
Ishan Karunaratne <ishansasika@gmail.com>
2020-10-08 10:19:28 +0530

    home page edit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <SignIn />
+    
 
   </React.StrictMode>,
   document.getElementById('root')
